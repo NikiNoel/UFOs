983d22ad00116813109f657bd5728b033e6f1f91
Niki <NikiNoel1218@gmail.com>
2022-08-31 12:18:37 -0400

    Overall completed, need to go back and edit filter buttons

---
+++ static/js/D3.js
@@ -0,0 +1,16 @@
+<div class="col-md-9">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+                <th>Date</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Country</th>
+                <th>Shape</th>
+                <th>Duration</th>
+                <th>Comments</th>            
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+</div>
